bf2ef05d0b6e1df87bda1c722b47f4a9fe670ec7
lizabespalova <bespalovalizv@gmail.com>
2024-03-09 01:07:08 +0200

    check

--- src/main/java/com/studentportal/helpbot/service/command/callbackquerycommands/BotHasQueryCommand.java
+++ src/main/java/com/studentportal/helpbot/service/command/callbackquerycommands/BotHasQueryCommand.java
@@ -1,8 +1,6 @@
 package com.studentportal.helpbot.service.command.callbackquerycommands;
 
 import com.studentportal.helpbot.service.command.Command;
-import org.springframework.stereotype.Component;
 
-@Component
 public interface BotHasQueryCommand extends Command {
 }
--- src/main/java/com/studentportal/helpbot/service/command/hasnotnullmessagecommands/BotHasNotNullMessageCommand.java
+++ src/main/java/com/studentportal/helpbot/service/command/hasnotnullmessagecommands/BotHasNotNullMessageCommand.java
@@ -1,9 +1,7 @@
 package com.studentportal.helpbot.service.command.hasnotnullmessagecommands;
 
 import com.studentportal.helpbot.service.command.Command;
-import org.springframework.stereotype.Component;
 
-@Component
 public interface BotHasNotNullMessageCommand extends Command {
 
 }
