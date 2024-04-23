0ab27cd780666adb5b3e3ffa72eb4e0b4b98acef
lizabespalova <bespalovalizv@gmail.com>
2024-04-23 16:23:57 +0300

    payment

--- src/main/java/com/studentportal/helpbot/service/command/callbackquerycommands/YesHasQueryCommand.java
+++ src/main/java/com/studentportal/helpbot/service/command/callbackquerycommands/YesHasQueryCommand.java
@@ -1,33 +1,28 @@
 package com.studentportal.helpbot.service.command.callbackquerycommands;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 import com.studentportal.helpbot.model.*;
 import com.studentportal.helpbot.repository.CustomerRepository;
 import com.studentportal.helpbot.repository.PurchaseRepository;
 import com.studentportal.helpbot.repository.RoomsRepository;
 import com.studentportal.helpbot.service.consts.Text;
 import com.studentportal.helpbot.service.dopclasses.CustomerActions;
-import com.studentportal.helpbot.service.dopclasses.ParameterStringBuilder;
 import com.studentportal.helpbot.service.mainclasses.Helpbot;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.telegram.telegrambots.meta.api.methods.invoices.CreateInvoiceLink;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.api.objects.payments.LabeledPrice;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
 import java.util.*;
 
 @Component
@@ -147,28 +142,16 @@ public class YesHasQueryCommand extends QueryCommands {
             String sign = md5(dataToString(data) + secret_key);
             data.put("sign", sign);
 
-            // Send post request
-            con.setDoOutput(true);
-            DataOutputStream wr = new DataOutputStream(con.getOutputStream());
-            wr.writeBytes(ParameterStringBuilder.getParamsString(data));
-            wr.flush();
-            wr.close();
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
 
-            int responseCode = con.getResponseCode();
-            System.out.println("Response Code : " + responseCode);
+            // Create request entity
+            HttpEntity<String> requestEntity = new HttpEntity<>(dataToString(data), headers);
 
-            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-            String inputLine;
-            StringBuffer response = new StringBuffer();
-
-            while ((inputLine = in.readLine()) != null) {
-                response.append(inputLine);
-            }
-            in.close();
-
-            // Print result
-            invoiceLink = response.toString();
-            System.out.println(response.toString());
+            // Send request
+            RestTemplate restTemplate = new RestTemplate();
+            ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.POST, requestEntity, String.class);
+            invoiceLink = response.getBody();
         } catch (Exception e) {
             e.printStackTrace();
         }
